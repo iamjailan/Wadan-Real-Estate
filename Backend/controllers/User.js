703aa3f751fb4ea9cb62b78daf0cf82f7adbdf56
Jailan Mirkhel <ahmadjailan2012@gmail.com>
2023-11-13 17:45:08 +0430

    Sign in functionality added

--- Backend/controllers/User.js
+++ Backend/controllers/User.js
@@ -1,5 +1,7 @@
 import User from "../models/UserModel.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 const signUp = async (req, res, next) => {
   const { email, password, username } = req.body;
@@ -13,8 +15,22 @@ const signUp = async (req, res, next) => {
   }
 };
 
-const signIn = (req, res) => {
-  res.json({ msg: "login User Routes" });
+const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!email) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) next(errorHandler(401, "Invalid credentials"));
+    const { password: pass, ...rest } = validUser._doc;
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECERT);
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export { signIn, signUp };
--- Backend/routes/AuthRoute.js
+++ Backend/routes/AuthRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signUp } from "../controllers/User.js";
+import { signIn, signUp } from "../controllers/User.js";
 
 const authRouter = express.Router();
 
 authRouter.route("/sign-up").post(signUp);
+authRouter.route("/sign-in").post(signIn);
 
 export default authRouter;
