161e8862a7818077fadc23d706f94038223a12bc
Jailan Mirkhel <ahmadjailan2012@gmail.com>
2023-12-18 11:40:08 +0430

    deploing error is fixed

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,9 @@ import authRouter from "./routes/AuthRoute.js";
 import userRouter from "./routes/UserRoute.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/ListingRoute.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -16,6 +19,12 @@ const PORT = 3579;
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
