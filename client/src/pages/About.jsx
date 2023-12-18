d503709b0f7ed431d483868543689b56624e5b02
Jailan Mirkhel <ahmadjailan2012@gmail.com>
2023-12-18 11:59:08 +0430

    error

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,4 +1,4 @@
-export default function About() {
+const About = () => {
   return (
     <div className="py-20 px-4 max-w-6xl mx-auto">
       <h1 className="text-3xl font-bold mb-4 text-slate-800">
@@ -26,4 +26,6 @@ export default function About() {
       </p>
     </div>
   );
-}
+};
+
+export default About;
