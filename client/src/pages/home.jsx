bd45351954efbcb9a16c229399aa8f4fe8ff600e
Jailan Mirkhel <ahmadjailan2012@gmail.com>
2023-12-19 15:17:44 +0430

    Home page got update

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -48,8 +48,8 @@ export default function Home() {
     <div>
       {/* top */}
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
-        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
-          Find your next <span className="text-slate-500">perfect</span>
+        <h1 className="text-slate-500 font-bold text-3xl lg:text-6xl">
+          Find your next <span className="text-slate-900">perfect</span>
           <br />
           place with ease
         </h1>
